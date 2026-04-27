1c9364a039a1ac93e799ccebaccf0777e2e9beb1
nayasky94 <37323952+nayasky94@users.noreply.github.com>
2026-04-27 12:16:47 +0900

    feat: 인증 API 구현 (회원가입/로그인/토큰 갱신)

---
+++ src/main/java/com/redline/jj/api/auth/dto/SignupRequest.java
@@ -0,0 +1,25 @@
+package com.redline.jj.api.auth.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupRequest {
+
+    @NotBlank
+    @Size(min = 4, max = 20)
+    private String userId;
+
+    @NotBlank
+    @Size(min = 8, max = 50)
+    private String userPw;
+
+    @NotBlank
+    @Size(max = 20)
+    private String userName;
+}
